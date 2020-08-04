980ed05b8099b34bf29454fefa3495ed12286ede
rafaelsoftcam <63867846+rafaelsoftcam@users.noreply.github.com>
2020-08-04 17:40:55 -0300

    Create banco.php

---
+++ banco.php
@@ -0,0 +1,21 @@
+<?php
+
+    $classe = new Classe();
+
+    class Classe
+    {
+        public function __construct()
+        {
+            echo "<dt>Html</dt>";
+            echo "<dd> Html Basico</dd>";
+            echo "<dd> Html Avançado</dd>";
+            echo "<dd> Html Mestre</dd>";
+            echo "<dt> PHP</dt>";
+            echo "<dd> PHP Básico</dd>";
+            echo "<dd> PHP Intermediário</dd>";
+            echo "<dd> PHP Avançado</dd>";
+            echo "</dl>";
+        }
+    }
+
+?>
